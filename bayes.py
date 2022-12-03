0784f4b0db52ee6e4b5e27561877f54552ab3813
Julian Cooper <jelc@stanford.edu>
2022-12-02 17:05:45 -0800

    Updated code to incorporate karthik's features

--- bayes.py
+++ bayes.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
 
 from common import load_data, save_pickle
 from sklearn.metrics import mean_squared_error
@@ -22,24 +23,6 @@ def create_summary_dfs(data):
         tmp = pd.DataFrame(data[bc]['summary'])
         output_df.append(tmp)
     return output_df
-
-
-def create_features(dfs):
-    """[PLACEHOLDER FUNCTION FOR KARTHIK"""
-    x1_nominal_capacity = []
-    x2_average_temperature = []
-    x3_qd_pct_change_first100 = []
-
-    for df in dfs:
-        x1_nominal_capacity.append(df.loc[1,'QD'])
-        x2_average_temperature.append(df.loc[1,'Tavg'])
-        x3_qd_pct_change_first100.append((df.loc[100,'QD']-df.loc[1,'QD'])/df.loc[1,'QD'])
-
-    return pd.DataFrame({
-        'x1': np.array(x1_nominal_capacity),
-        'x2': np.array(x2_average_temperature),
-        'x3': np.array(x3_qd_pct_change_first100)
-    })
 
 
 def exp_decay_function(x, params):  # alpha=0.2, beta=2, gam=0.4; # alpha=0.000005, beta=1.44, gam=15000
@@ -69,45 +52,43 @@ def fetch_model(x, params, model_id):
     return y_pred
 
 
-def get_pred(data, params, model_id, scale=1000):
+def get_pred(num_cycles, params, model_id, scale=1000):
     """Helper function to access predictions for discharge capacity"""
-    cycle_life = int(data['cycle_life'][0]) - 2
-    y_true = np.array(data['summary']['QD'][1:(cycle_life+1)])
-    x = np.linspace(0, cycle_life, cycle_life) / scale  # / cycle_life
+    # cycle_life = int(data['cycle_life'][0]) - 2
+    # y_true = np.array(data['summary']['QD'][1:(cycle_life+1)])
+    x = np.linspace(0, num_cycles, num_cycles) / scale  # / cycle_life
     y_pred = fetch_model(x, params, model_id)
 
-    return y_pred, y_true
+    return y_pred
 
 
-def get_rul(data, params, model_id, threshold=0.8):
+def get_rul(threshold, params, model_id):
     """Helper function to access prediction for remianing useful life"""
-    cycle_life = int(data['cycle_life'][0]) - 2
-    nominal = data['summary']['QD'][1]
-    y_val = nominal.copy()
-    cycle_count = 0
-    while (y_val / nominal) > threshold:
-        cycle_count += 1
-        y_val = fetch_model(cycle_count, params, model_id)
-        # print(y_val)
-    y_pred = cycle_count
-    y_true = cycle_life
+    # cycle_life = int(data['cycle_life'][0]) - 2
+    # nominal = data['summary']['QD'][1]
+    y_val = params[2]  # nominal
+    count = 0
+    while y_val > threshold:
+        count += 1
+        y_val = fetch_model(count, params, model_id)
+    y_pred = count
 
-    return y_pred, y_true
+    return y_pred
 
 
-def evaluate_fit(y_true, params, model_id, start_idx=100):
+def evaluate_fit(y_test, params, model_id, start_idx=100, scale=1000):
     """Compute mse for each battery cell curve"""
-    i = 0
     mse_store = []; rul_mape_store = []
-    for bc in data.keys():
-        params_bc = [params[0][i], params[1][i], params[2][i]]
-        y_pred, y_true = get_pred(data[bc], params_bc, model_id)
-        rul_pred, rul_true = get_rul(data[bc], params_bc, model_id)
+    for i in range(len(y_test)):
+        cycle_life, threshold = len(y_test[i]), y_test[i][-1]
+        params_i = [params[0][i], params[1][i], params[2][i]]
+
+        y_pred, y_true = get_pred(cycle_life, params_i, model_id), y_test[i]
+        rul_pred, rul_true = get_rul(threshold, params_i, model_id), cycle_life/scale
+
         mse = mean_squared_error(y_pred[start_idx:], y_true[start_idx:])
         rul_mape = mean_absolute_percentage_error(np.array([rul_pred]), np.array([rul_true]))
         mse_store.append(mse); rul_mape_store.append(rul_mape)
-
-        i += 1
 
     return mse_store, rul_mape_store
 
@@ -117,9 +98,11 @@ def plot_examples(data, ex_list, params, model_id, scale=1000):
     i = 0
     for bc in data.keys():
         if bc in ex_list:
-            params_bc = [params[0][i], params[1][i], params[2][i]]
-            cycle_life = int(data[bc]['cycle_life'][0])-2
-            y_pred, y_true = get_pred(data[bc], params_bc, model_id)
+            y_test_i = data[bc]['summary']['QD'][1:].copy()
+            cycle_life = len(y_test_i)  # y_test_i[-1]
+            params_i = [params[0][i], params[1][i], params[2][i]]
+
+            y_pred, y_true = get_pred(cycle_life, params_i, model_id), y_test_i
             x = np.linspace(0, cycle_life, cycle_life) / scale
 
             plt.scatter(x, y_true, color='grey')
@@ -132,27 +115,49 @@ def plot_examples(data, ex_list, params, model_id, scale=1000):
         i += 1
 
 
-def prepare_data_for_stan(dfs):
-    """Create dictionary of data inputs for stan model"""
-    y = []
-    N_BC = []
+def create_features(train_data):
+    """[PLACEHOLDER FUNCTION FOR KARTHIK"""
+    X_df = train_data.groupby(['bat_id']).last().reset_index(drop=True)
+    X_df.columns = X_df.columns.str.lower()
 
-    for df in dfs:
-        N_BC.append(df.shape[0])
-        y.append(np.array(df['QD']))
-    X = create_features(dfs)  # check dim!
-    y = np.hstack(y)
+    # for df in dfs:
+    #     x1_nominal_capacity.append(df.loc[1,'QD'])
+    #     x2_average_temperature.append(df.loc[1,'Tavg'])
+    #     x3_qd_pct_change_first100.append((df.loc[100,'QD']-df.loc[1,'QD'])/df.loc[1,'QD'])
+
+    x1_nominal_capacity = train_data.groupby(['bat_id']).nth(1)['QD']
+    x2_variance_100v10 = X_df['variance'].copy()
+    x3_log_min_difference = X_df['log|min(delta(q(v)))|'].copy()
+    x4_chargetime_average = X_df['chargetimeavg cyc1-5'].copy()
+    x5_avg_temperature_integral = X_df['temp integral'].copy()
+    # x6_intercept_2to100 = X_df['intercept 2-100'].copy()
+
+    X = pd.DataFrame({
+        'x1': np.array(x1_nominal_capacity),
+        'x2': np.array(x2_variance_100v10),
+        'x3': np.array(x3_log_min_difference),
+        'x4': np.array(x4_chargetime_average),
+        'x5': np.array(x5_avg_temperature_integral),
+        # 'x6': np.array(x6_intercept_2to100),
+    })
+
+    X_scaled = scale(X)
+    X_scaled[:, 0] = np.array(x1_nominal_capacity)  # overwrite nominal capacity
+    return X_scaled
+
+
+def prepare_data_for_stan(X_scaled, train_y):
+    """Create dictionary of data inputs for stan model"""
+    labels = np.hstack(train_y)
+    N_BC = np.array([len(y_i) for y_i in train_y])
 
     return {
         'T': np.sum(N_BC),               # total number of cycles
-        'N': len(dfs),
-        'd': X.shape[1],
-        'y': y,                          # flattened labels
-        'x1': np.array(X['x1']),
-        'x2': np.array(X['x2']),
-        'x3': np.array(X['x3']),
-        # 'X': X.to_json(),                # feature variables
-        'N_BC': np.array(N_BC)
+        'N': len(train_y),
+        'd': X_scaled.shape[1],
+        'y': labels,                          # flattened labels
+        'X': X_scaled,
+        'N_BC': N_BC
     }
 
 
@@ -182,26 +187,13 @@ def prepare_code_for_stan():
         int<lower=0> N;             // number of battery cells = 200
         int<lower=0> d;             // number of feature dimensions
         vector[T] y;
-        vector[N] x1;
-        vector[N] x2;
-        vector[N] x3;
+        matrix[N,d] X;
         array[N] int N_BC;          // cycle life for each battery cell
     }
     parameters {
-        real alpha_0;
-        real alpha_1;
-        real alpha_2;
-        real alpha_3;
-
-        real beta_0;
-        real beta_1;
-        real beta_2;
-        real beta_3;
-
-        //real gamma_0;
-        //real gamma_1;
-        //real gamma_2;
-        //real gamma_3;
+        real a_0, a_1, a_2, a_3, a_4, a_5;
+        real b_0, b_1, b_2, b_3, b_4, b_5;
+        //real g_0, g_1;
 
         real<lower=0> sigma;
     }
@@ -215,9 +207,9 @@ def prepare_code_for_stan():
         real scaled_cycle_count;
 
         for(i in 1:N) {
-            alpha[i] = alpha_0 + alpha_1*x1[i] + alpha_2*x2[i] + alpha_3*x3[i];
-            beta[i] = beta_0 + beta_1*x1[i] + beta_2*x2[i] + beta_3*x3[i];
-            gamma[i] = x1[i];
+            alpha[i] = a_0 + a_1*X[i,1] + a_2*X[i,2] + a_3*X[i,3] + a_4*X[i,4];
+            beta[i] = b_0 + b_1*X[i,1] + b_2*X[i,2] + b_3*X[i,3] + b_4*X[i,4];
+            gamma[i] = X[i,1];
 
             for (j in 1:N_BC[i]) {
                 scaled_cycle_count = j / 1000.0;
@@ -229,20 +221,14 @@ def prepare_code_for_stan():
     }
     }
     model {
-        alpha_0 ~ normal(2.5, 1);
-        alpha_1 ~ normal(0, 1);
-        alpha_2 ~ normal(0, 1);
-        alpha_3 ~ normal(0, 1);
+        a_0 ~ normal(2.5, 1);
+        a_1, a_2, a_3, a_4 ~ normal(0, 1);
 
-        beta_0 ~ normal(2.5, 1);
-        beta_1 ~ normal(0, 1);
-        beta_2 ~ normal(0, 1);
-        beta_3 ~ normal(0, 1);
+        b_0 ~ normal(2.5, 1);
+        b_1, b_2, b_3, b_4 ~ normal(0, 1);
 
-        //gamma_0 ~ normal(1.1, 1);
-        //gamma_1 ~ normal(0, 1);
-        //gamma_2 ~ normal(0, 1);
-        //gamma_3 ~ normal(0, 1);
+        //g_0 ~ normal(1.1, 1);
+        //g_1 ~ normal(0, 1);
 
         sigma ~ gamma(1, 2);
 
@@ -292,13 +278,34 @@ def test_script_for_stan():
     return "Test script runs without error!"
 
 
-def prepare_params_given_samples(fit, stan_data):
+def prepare_params_given_samples(fit, X):
     """Helper function to pull together mle estimates given posterior samples"""
-    alpha = fit['alpha_1'].mean()*stan_data['x1'] + fit['alpha_2'].mean()*stan_data['x2'] + fit['alpha_3'].mean()*stan_data['x3'] + fit['alpha_0'].mean()
-    beta = fit['beta_1'].mean()*stan_data['x1'] + fit['beta_2'].mean()*stan_data['x2'] + fit['beta_3'].mean()*stan_data['x3'] + fit['beta_0'].mean()
-    gamma = stan_data['x1']
+    a_0, a_1, a_2, a_3, a_4, a_5 = fit['a_0'].mean(), fit['a_1'].mean(), fit['a_2'].mean(), fit['a_3'].mean(), fit['a_4'].mean(), fit['a_5'].mean()
+    b_0, b_1, b_2, b_3, b_4, b_5 = fit['b_0'].mean(), fit['b_1'].mean(), fit['b_2'].mean(), fit['b_3'].mean(), fit['b_4'].mean(), fit['b_5'].mean()
+    # g_0, g_1 = fit['g_i'].mean(), fit['g_0'].mean()
+
+    alpha = a_0 + a_1*X[:, 0] + a_2*X[:, 1] + a_3*X[:, 2] + a_4*X[:, 3] + a_5*X[:, 4]
+    beta = b_0 + b_1*X[:, 0] + b_2*X[:, 1] + b_3*X[:, 2] + b_4*X[:, 3] + b_5*X[:, 4]
+    gamma = X[:, 0]  # g_0 + g_1*X_scaled[:, 0]
 
     return [alpha, beta, gamma]
+
+
+def prepare_label_data(data_dict, train_ids, test_ids):
+    """Helper function to prepare labels for train and test"""
+    train_y, test_y = [], []
+    for bc in data_dict:
+
+        if bc in train_ids:
+            train_y.append(data_dict[bc]['summary']['QD'][1:])
+
+        elif bc in test_ids:
+            test_y.append(data_dict[bc]['summary']['QD'][1:])
+
+        else:
+            print("Error: we are missing some battery ids!")
+
+    return train_y, test_y
 
 
 if __name__ == '__main__':
@@ -306,26 +313,32 @@ if __name__ == '__main__':
     # load data
     infile = sys.argv[1]
     data = load_data(os.path.join('data', infile))
-    train = pd.read_csv(os.path.join('data', 'train.csv'))
-    test = pd.read_csv(os.path.join('data', 'test.csv'))
-    dfs = create_summary_dfs(data)
+    train_dat = pd.read_csv(os.path.join('data', 'train.csv'))
+    test_dat = pd.read_csv(os.path.join('data', 'test.csv'))
+
+    train_bat_ids = np.array(train_dat['bat_id'].drop_duplicates())
+    test_bat_ids = np.array(test_dat['bat_id'].drop_duplicates())
+    y_train, y_test = prepare_label_data(data, train_bat_ids, test_bat_ids)
 
     # bayes model
     # print(test_script_for_stan())
     stan_code = prepare_code_for_stan()
-    stan_data = prepare_data_for_stan(dfs)
+    X_train = create_features(train_dat)
+    stan_data = prepare_data_for_stan(X_train, y_train)
     posterior = stan.build(stan_code, data=stan_data, random_seed=101)
     fit = posterior.sample(num_samples=1000, num_chains=1)
     save_pickle(posterior, filename='model.pkl')
     save_pickle(fit, filename='fit.pkl')
 
     # evaluate fit
+    n = len(y_test)
     map = {
-        1: (0.2, 2, 0.4),  # alpha, beta, gam
-        2: (2.5, 2.5, 1.1),  # shape, midpoint, asymptote
+        1: [np.ones(n)*0.2, np.ones(n)*2, np.ones(n)*0.4],  # alpha, beta, gam
+        2: [np.ones(n)*2.5, np.ones(n)*2.5, np.ones(n)*1.1],  # shape, midpoint, asymptote
     }
-    params = prepare_params_given_samples(fit, stan_data)  # params = map[MODEL_ID]
-    mse_store, rul_mape_store = evaluate_fit(data, params=params, model_id=MODEL_ID)
+    X_test = create_features(test_dat)
+    params = prepare_params_given_samples(fit, X_test)  # params = map[MODEL_ID]
+    mse_store, rul_mape_store = evaluate_fit(y_test, params=params, model_id=MODEL_ID)
 
     # write results
     param_list = [
